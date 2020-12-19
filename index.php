85125d32147825906d0a408f1ba6736bb2d6cd89
rut <p.mroz.zywiol@gmail.com>
2020-12-19 23:00:41 +0100

    basic auth simulation

--- index.php
+++ index.php
@@ -15,27 +15,61 @@
     </div>
 
     <div class="loginForm">
-
-    
         <div class="container-fluid" id="authForm">
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-            </div> 
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Password</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="password">
-            </div>
-             <div class="controlButtons">
-                <a href="./resetView.html">Reset Password</a>
-             </div>
-             <button type="submit" class="btn btn-primary">Login</button>
+            <form>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="emailInput" placeholder="name@example.com">
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div> 
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="passwordInput" placeholder="password">
+                 </div> 
+            
+                <div class="d-flex justify-content-right h-100">
+                    <a href="./resetView.html">Reset Password</a>
+                </div>
+                <div class="d-flex justify-content-center h-100">
+                    <button id="login" type="submit" class="btn btn-primary">Login</button>
+                </div>
+             </form>
+             
         </div>
     </div>
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+    $(function(){
+		$('#login').click(function(e){
 
-    
+			var valid = this.form.checkValidity();
+
+			if(valid){
+				var username = $('#emailInput').val();
+				var password = $('#passwordInput').val();
+			}
+
+			e.preventDefault();
+
+			$.ajax({
+				type: 'POST',
+				url: 'auth.php',
+				data:  {username: username, password: password},
+				success: function(data){
+					alert(data);
+					if($.trim(data) === "1"){
+						setTimeout(' window.location.href =  "index.php"', 1000);
+					}
+				},
+				error: function(data){
+					alert('there were erros while doing the operation.');
+				}
+			});
+
+		});
+	});
+</script>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </body>
