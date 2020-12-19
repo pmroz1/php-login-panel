c9c4c230a8906105dc47cc86f9d059c8a5235bcf
rut <p.mroz.zywiol@gmail.com>
2020-12-19 01:13:42 +0100

    env set up

---
+++ index.php
@@ -0,0 +1,17 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Panel</title>
+</head>
+
+<body>
+
+<?php 
+    echo  "PHello World!";
+?>
+
+</body>
+
+</html>
